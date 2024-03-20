22599f4c7217b253d0a5beb1e9d4c856d5e77a1a
iamtheuri <theuridavid56@gmail.com>
2024-03-20 17:58:50 +0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Homecards from "./components/Homecards";
+import JobListings from "./components/JobListings";
+import ViewAllJobs from "./components/ViewAllJobs";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Homecards />
+      <JobListings />
+      <ViewAllJobs />
+    </>
+  );
+};
+export default App;
