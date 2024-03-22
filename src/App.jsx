b332902875941edb69ec961f19f5b31f6054a821
iamtheuri <theuridavid56@gmail.com>
2024-03-22 03:03:26 +0300

    Routing complete, JSON server set up and data fetching is as required

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Homecards from "./components/Homecards";
-import JobListings from "./components/JobListings";
-import ViewAllJobs from "./components/ViewAllJobs";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import MainLayout from "./layouts/MainLayout";
+import JobsPage from "./pages/JobsPage";
+import NotFoundPage from "./pages/NotFoundPage";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<Homepage />} />
+      <Route path="/jobs" element={<JobsPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Route>
+  )
+);
 
 const App = () => {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <Homecards />
-      <JobListings />
-      <ViewAllJobs />
-    </>
-  );
+  return <RouterProvider router={router} />;
 };
 export default App;
